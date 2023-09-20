4163acbfef21fde2d7a76b93f75cefdf022f67b4
Dhemas25 <dhemaswicaksono@gmail.com>
2023-09-20 15:55:56 +0700

    Selesai tutorial 3

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -76,7 +76,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shopping_list.wsgi.application'
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
